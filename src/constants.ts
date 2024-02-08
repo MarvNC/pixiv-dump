c61125e8a8f16e5bbfacdc87b158c8e561a8f429
MarvNC <rotrobmin@gmail.com>
2024-02-08 00:36:13 -0800

    Add PIXIV_CATEGORIES constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,19 @@
 export const PIXIV_BASE_URL = 'https://dic.pixiv.net/';
 export const DB_FILENAME = 'database.db';
 export const DB_FOLDER = 'db';
+export const PIXIV_CATEGORIES = [
+  'アニメ',
+  'マンガ',
+  'ラノベ',
+  'ゲーム',
+  'フィギュア',
+  '音楽',
+  'アート',
+  'デザイン',
+  '一般',
+  '人物',
+  'キャラクター',
+  'セリフ',
+  'イベント',
+  '同人サークル',
+];
