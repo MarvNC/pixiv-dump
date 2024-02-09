2918a7a1214ed3483ba8502c550a4df5d6e74423
MarvNC <rotrobmin@gmail.com>
2024-02-08 22:20:34 -0800

    Add yargs package and implement timeout functionality and graceful exit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
 import { PIXIV_CATEGORIES } from './constants';
 import { getCategoryLastScraped } from './helpers/lastScrapedHandler';
 import { scrapePixivCategory } from './scrape/scrapePixivCategory';
@@ -7,6 +9,34 @@ import { scrapeAllReadings } from './scrapeAllReadings';
 export const prisma = new PrismaClient();
 
 (async () => {
+  const argv = yargs(hideBin(process.argv)).option('timeout', {
+    describe: 'Exit program after a specified amount of time (in milliseconds)',
+    type: 'number',
+  }).argv;
+
+  const { timeout } = argv as { timeout: number };
+
+  if (timeout) {
+    console.log(`Timeout set to ${timeout} milliseconds`);
+    setTimeout(async () => {
+      console.log('Timeout reached. Exiting program');
+      await prisma.$disconnect();
+      process.exit(0);
+    }, timeout);
+  }
+
+  scrapeAll()
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+})();
+
+async function scrapeAll() {
   for (const category of PIXIV_CATEGORIES) {
     const newestScrapedDate = await getCategoryLastScraped(category);
     console.log(`Scraping ${category} from ${newestScrapedDate}`);
@@ -22,12 +52,12 @@ export const prisma = new PrismaClient();
     where: { lastScrapedReading: { not: null } },
   });
   console.log(`Total articles scraped: ${totalArticlesWithReading}`);
-})()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+}
+
+process.on('SIGINT', async () => {
+  console.log(
+    'Received SIGINT signal. Disconnecting Prisma and exiting program',
+  );
+  await prisma.$disconnect();
+  process.exit(0);
+});
