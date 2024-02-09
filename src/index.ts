5277fa70609db87f367e298e9affca2763357756
MarvNC <rotrobmin@gmail.com>
2024-02-08 21:56:40 -0800

    Add scrape all readings

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { PIXIV_CATEGORIES } from './constants';
 import { getCategoryLastScraped } from './helpers/lastScrapedHandler';
 import { scrapePixivCategory } from './scrape/scrapePixivCategory';
+import { scrapeAllReadings } from './scrapeAllReadings';
 
 export const prisma = new PrismaClient();
 
@@ -14,6 +15,13 @@ export const prisma = new PrismaClient();
   console.log('Scraping of article summaries complete');
   const totalArticles = await prisma.pixivArticle.count();
   console.log(`Total articles: ${totalArticles}`);
+
+  await scrapeAllReadings();
+  console.log('Scraping of article readings complete');
+  const totalArticlesWithReading = await prisma.pixivArticle.count({
+    where: { lastScrapedReading: { not: null } },
+  });
+  console.log(`Total articles scraped: ${totalArticlesWithReading}`);
 })()
   .then(async () => {
     await prisma.$disconnect();
