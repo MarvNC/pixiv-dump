9ecec2b2e0d54094fb0842b65b7ba3670490cd7c
MarvNC <rotrobmin@gmail.com>
2024-02-10 11:39:40 -0800

    Prettier

--- src/scrape/scrapePixivCategory.ts
+++ src/scrape/scrapePixivCategory.ts
@@ -2,17 +2,21 @@ import cliProgress from 'cli-progress';
 import { findPageNumberAtDate } from '../helpers/findPageNumberAtDate';
 import { scrapeArticleList } from './scrapeArticleList';
 
-export async function scrapePixivCategory(category: string, lastScraped: string) {
+export async function scrapePixivCategory(
+  category: string,
+  lastScraped: string,
+) {
   const pageNumberToStartAt = await findPageNumberAtDate(category, lastScraped);
   console.log(`${category}:Starting at page ${pageNumberToStartAt}`);
   const progressBar = new cliProgress.SingleBar(
     {
-      format: 'Progress [{bar}] {percentage}% | ETA: {eta}s | {value}/{total} Pages',
+      format:
+        'Progress [{bar}] {percentage}% | ETA: {eta}s | {value}/{total} Pages',
       barCompleteChar: '\u2588',
       barIncompleteChar: '\u2591',
       hideCursor: true,
     },
-    cliProgress.Presets.shades_classic
+    cliProgress.Presets.shades_classic,
   );
 
   progressBar.start(pageNumberToStartAt, 0);
