ba5966a6bdb6a209212c24c3d874cc27ed9c4d66
MarvNC <rotrobmin@gmail.com>
2024-02-10 19:37:23 -0800

    Format

--- src/helpers/getTotalPageCount.ts
+++ src/helpers/getTotalPageCount.ts
@@ -3,7 +3,7 @@ import fetchPixivPage from '../fetch/fetchPixivPage';
 export async function getTotalPageCount(category: string) {
   const firstPageData = await fetchPixivPage(category, 1);
   const totalPageCount = Math.ceil(
-    firstPageData.meta.all_count / firstPageData.meta.count
+    firstPageData.meta.all_count / firstPageData.meta.count,
   );
   return totalPageCount;
 }
