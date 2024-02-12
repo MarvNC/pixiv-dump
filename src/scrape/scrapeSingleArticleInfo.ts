f41024e45bfe29ff7a2e67feaecef146660f59ed
MarvNC <rotrobmin@gmail.com>
2024-02-12 14:01:10 -0800

    Refactor scrapeSingleArticleInfo function and add getMainText function

--- src/scrape/scrapeSingleArticleInfo.ts
+++ src/scrape/scrapeSingleArticleInfo.ts
@@ -8,10 +8,10 @@ export async function scrapeSingleArticleInfo(tag_name: string) {
   const url = pixivArticleURL(tag_name);
   const response = await fetchURL(url);
   const dom = new JSDOM(response.data);
-  // Get the reading
-  const reading = getReading(dom);
-  // Get headers
-  const header = getHeaders(dom);
+  const document = dom.window.document;
+  const reading = getReading(document);
+  const header = getHeaders(document);
+  const mainText = getMainText(document);
 
   // Update the article
   await prisma.pixivArticle.update({
@@ -24,14 +24,52 @@ export async function scrapeSingleArticleInfo(tag_name: string) {
   });
 }
 
-function getHeaders(dom: JSDOM) {
-  const breadcrumbs = dom.window.document.getElementById('breadcrumbs');
+function getHeaders(document: Document) {
+  const breadcrumbs = document.getElementById('breadcrumbs');
   const header = breadcrumbs
     ? [...breadcrumbs.children].map((child) => child.textContent)
     : [];
   return header;
 }
 
-function getReading(dom: JSDOM) {
-  return dom.window.document.querySelector('p.subscript')?.textContent;
+function getReading(document: Document) {
+  return document.querySelector('p.subscript')?.textContent || '';
+}
+
+function getMainText(document: Document) {
+  const articleBody = document.getElementById('article-body');
+  if (!articleBody) {
+    return '';
+  }
+
+  // Clean out article index/misc
+  articleBody.querySelector('.article-index')?.remove();
+
+  const gaiyou = [...articleBody.querySelectorAll('h2')].find((h2) => {
+    return h2.textContent?.includes('概要');
+  });
+  let mainText = '';
+  if (gaiyou) {
+    mainText = getTextUntilH2(gaiyou.nextElementSibling);
+  } else {
+    // No gaiyou, just get the first paragraph
+    mainText = getTextUntilH2(articleBody.firstElementChild);
+  }
+  return mainText.trim();
+}
+
+function getTextUntilH2(start: Element | null) {
+  if (!start) {
+    return '';
+  }
+  let text = '';
+  let next: Element | null = start;
+  while (next && next.tagName !== 'H2') {
+    const cleanText = next.textContent?.trim();
+    if (cleanText && next.tagName === 'P') {
+      text += cleanText + '\n';
+    }
+    next = next.nextElementSibling;
+  }
+  return text;
 }
