222c09bf3d0e8d3c3b07454a2031ee09b64ded35
MarvNC <rotrobmin@gmail.com>
2024-02-12 14:03:08 -0800

    Add mainText column

---
+++ prisma/migrations/20240212220220_add_main_text/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "PixivArticle" ADD COLUMN "header" TEXT;
+ALTER TABLE "PixivArticle" ADD COLUMN "lastScrapedReading" TEXT;
+ALTER TABLE "PixivArticle" ADD COLUMN "mainText" TEXT;
+ALTER TABLE "PixivArticle" ADD COLUMN "reading" TEXT;
