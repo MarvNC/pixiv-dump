7cae8604b4f96b3c46d5b6bab3a2315672ba5175
MarvNC <rotrobmin@gmail.com>
2024-02-16 13:30:19 -0800

    Get outdated articles

--- src/scrape/getArticlesToScrape.ts
+++ src/scrape/getArticlesToScrape.ts
@@ -1,10 +1,23 @@
 import { prisma } from '..';
 
 export async function getArticlesToScrape() {
-  return await prisma.pixivArticle.findMany({
-    where: { lastScrapedArticle: null },
-    select: {
-      tag_name: true,
-    },
+  return (
+    await prisma.pixivArticle.findMany({
+      where: { lastScrapedArticle: null },
+      select: {
+        tag_name: true,
+        updated_at: true,
+        lastScrapedArticle: true,
+      },
+    })
+  ).filter((article) => {
+    const lastUpdatedDate = new Date(article.updated_at);
+    const lastScrapedArticle = new Date(article.lastScrapedArticle || 0);
+    return (
+      // If the article has not been scraped or it's been updated
+      // and the updated article has yet to be scraped
+      article.lastScrapedArticle === null ||
+      lastScrapedArticle < lastUpdatedDate
+    );
   });
 }
